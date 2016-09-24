88c871bcd8e12e0a2e4bb81e52a713175b3f47f9
Philippe Nizer <pnizer@gmail.com>
2016-09-24 14:59:18 -0300

    Exemplo

--- ex11-curry.groovy
+++ ex11-curry.groovy
@@ -1,7 +1,7 @@
 def mult = { x, y -> x * y }
 println mult(3, 5)
 
-def multPor10 = mult.ncurry(1, 10)
+def multPor10 = mult.curry(10)
 println multPor10(4)
 println multPor10(3)
 println multPor10(30)
